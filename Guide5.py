6581b69f00e7c09939dce655fed5279b4b208bd7
LaCajaVengadora <127139455+LaCajaVengadora@users.noreply.github.com>
2023-04-24 08:43:11 -0300

    Create Guide5.py

---
+++ Guide5.py
@@ -0,0 +1,15 @@
+#5A
+for i in range(5): #O bueno, n cantidad de veces
+    print("hola")
+    
+#5B
+val = int(input("Ingrese un num hasta el cual será la escala: ")); jump = int(input("Ingrese el salto: "))
+for i in range(0,val+1,jump):
+    print(i)
+#5C
+#5D
+#5E
+#5F
+#5G
+#5H
+#5I
