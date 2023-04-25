d540538cc4e62f52aa133eac6912b3b7c402c5ea
LaCajaVengadora <127139455+LaCajaVengadora@users.noreply.github.com>
2023-04-25 14:46:56 -0300

    Update Guide5.py

--- Guide5.py
+++ Guide5.py
@@ -30,14 +30,70 @@ n = int(input("Ingrese la cantidad de jugadores: "));
 for i in range(n):
     print(f"El jugador {i+1} sacó {randint(1,6)}")
     
-#5F -------------------------to-do------------
+#5F
 from random import randint
-n = int(input("Ingrese la cantidad de jugadores: "));
-val = int(input("Ingrese el valor a conseguir: ")); cont=False
+n = int(input("Ingrese la cantidad de jugadores: ")); val = int(input("Ingrese el valor a conseguir: ")); cont=False
 for i in range(n):
     jVal=randint(1,6) ;print(f"El jugador {i+1} sacó {jVal}")
-    if jVal==val: con=True
-if cont: print("Se ha conseguido el valor")
+    if jVal==val: cont=True
+if cont: print(f"Se ha conseguido el valor {val}")
+else: print("No se ha conseguido el valor")
+
 #5G
+from random import randint
+n = int(input("Ingrese la cantidad de dados: ")); high = 0
+for i in range(n):
+    jVal=randint(1,6); print(f"Dado {i+1}: {jVal}");
+    if jVal>high: 
+        high=jVal
+print(f"Valor más alto: {high}")
+
 #5H
+from random import randint
+puntaje = 0
+for i in range(10):
+    val=randint(1,20); 
+    if val%2==0: par=True
+    else: par=False
+    user = input(f"El número (vez {i+1}) es par? (y/n) ")
+    if user.upper()=="Y": uPar=True
+    else: uPar=False
+    if par and uPar: puntaje+=1
+    print(f"El número era {val}")
+print(f"Puntaje: {puntaje}")
+
 #5I
+alm = int(input("Ingrese la cantidad de alumnos: ")); pGen=0
+for i in range(alm):
+    pAlm=0
+    for j in {1,2,3}:
+        pAlm+=float(input(f"Ingrese la nota {j} del alumno {i+1}: "))
+    print(f"El promedio del alumno {i+1} es de {round(pAlm/3,2)}")
+    pGen+=pAlm/3
+print(f"El promedio general del curso es de {round(pGen/3,2)}")
+
+#5J
+cant = int(input("Ingrese la cantidad de empleados: ")); slTotal=0; mTotal=0; fTotal=0;
+for i in range(cant):
+    slTotal+=float(input(f"Ingrese el sueldo del empleado {i+1}: ")); sxEmp=input(f"Ingrese el sexo del empleado {i+1}: (M/F)")
+    if sxEmp.upper()=="M": mTotal+=1
+    elif sxEmp.upper()=="F": fTotal+=1
+    else:
+        print("Qué, algún intrasexual?"); break
+    
+print(f"Empleados de sexo masculino: {mTotal}\nEmpleados de sexo femenino: {fTotal}\nSueldo promedio: {slTotal/cant}")
+
+#5K
+cant = int(input("Ingrese la cantidad de alumnos: ")); lv1=0; lv2=0; lv3=0;
+for i in range(cant):
+    agno = int(input(f"Ingrese el número del año del alumno {i+1} (ej:1 para Primer Año): "))
+    if agno==1 or agno==2: lv1+=1
+    elif agno==3 or agno==4: lv2+=1
+    elif agno==5 or agno==6: lv3+=1
+    else: print("Algo anda mal..."); break
+    
+print(f"Alumnos en el nivel 1: {lv1}\nAlumnos en el nivel 2: {lv2}\nAlumnos en el nivel 3: {lv3}")
+
+#5L
+#5M
+#5N
