f05804afb5ed25546d028733d97f4dd74551090d
LaCajaVengadora <127139455+LaCajaVengadora@users.noreply.github.com>
2023-05-15 08:55:15 -0300

    Update Guide6.py

--- Guide6.py
+++ Guide6.py
@@ -10,10 +10,66 @@ while k!=30:
 print(n/30)
 
 #6B
+from random import randint
+times = int(input("Ingrese la cantidad de tiradas: ")); right=0; wrong=0
+for i in range(times):
+    num = randint(1,6)
+    choice = int(input(f"{i+1}.¿Qué n salió? "))
+    if choice==num:
+        right+=1; print(f"Correcto. Aciertos:{right}")
+    else:
+        wrong+=1; print(f"Incorrecto. Errores:{wrong}")
+print(f"De {times} tiradas, {right} fue/ron correcta/s y {wrong} no lo fue/ron")
+
 #6C
+seguir = input("Ingrese si quiere iniciar el programa: (y) ").upper()
+if seguir=="Y": seguir=True
+else: seguir=False
+while(seguir):
+    char = input("Ingrese el carácter a repetir: "); num = int(input(f"Ingrese la cantidad de veces a repetir {char}: "))
+    if num>50: 
+        print(f"{num} es mayor que 50"); break
+    for i in range(num):
+        print(char, end='')
+    print()
+    seguir = input("Ingrese si quiere continuar con el programa: (y) ").upper()
+    if seguir=="Y": seguir=True
+    else: seguir=False
+print("Fin")
+
 #6D
+n1 = int(input("Ingrese el primer número: "))
+n2 = int(input(f"Ingrese el segundo número (menor que {n1})"))
+while n2>=n1:
+    print(f"{n2} NO es menor que {n1}", end=' ');n2 = int(input(f"Ingrese el segundo número devuelta (menor que {n1})"))
+print(f"Los números son {n1} y {n2}")
+
 #6E
+Lista = []; divisores = 0
+n = int(input("Ingrese un número. Se escribiran los números primos entre el 1 y tal número: "))
+for i in range(n):
+    for j in range(i+1): 
+        if (i+1)%(j+1)==0: Lista.append(j+1)
+    for k in Lista: divisores+=1
+    if divisores==2 or divisores==1: print(i+1)
+    Lista = []; divisores = 0
+    
 #6F
+def getPrimos(n):
+    ex = []; full = []; divisores = 0
+    for i in range(n):
+        for j in range(i+1): 
+            if (i+1)%(j+1)==0: ex.append(j+1)
+        for k in ex: divisores+=1
+        if divisores==2 or divisores==1: full.append(i+1)
+        ex = []; divisores = 0
+    full.reverse()
+    return full
+
+num = int(input("Escriba un número mayor que 1: "))
+print("Descomposición en factores primos: ");
+for i in getPrimos(num):
+    pass
 #6G
 #6H
 #6I
