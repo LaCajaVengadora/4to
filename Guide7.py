c3031c0798a833fbb24c41b9f3f3cfae291a505a
LaCajaVengadora <127139455+LaCajaVengadora@users.noreply.github.com>
2023-06-26 09:00:17 -0300

    Update Guide7.py

--- Guide7.py
+++ Guide7.py
@@ -16,3 +16,38 @@ Mensaje(S,R,M,D)
 FtoC= lambda f:round((f-32)*5/9,2)
 CtoF= lambda c:round(c*9/5+32,2)
 print(FtoC(1));print(CtoF(-17.22))
+
+#7C
+from math import pi
+gradeToRadian = lambda g: round(g*pi/180,2)
+print(gradeToRadian(60))
+
+#7D
+perimeterOfSize = lambda sizes, length: sizes*length
+print(perimeterOfSize(4, 2))
+
+#7E
+def listOf(cant):
+    lista = []
+    for i in range(cant): lista.append(input(f"Ingrese el elemento {i+1}: "))
+    return lista
+print(listOf(2))
+
+#7F
+def makeRectangle(h,w,char):
+    for i in range(w):
+        for j in range(h):
+            print(char,end='')
+        print()
+makeRectangle(10,5,"#")
+
+#7G
+def makeTriangle(size):
+    lista = []
+    for i in range(size): lista.append("*")
+    firstHalf(lista);lastHalf(lista)
+def firstHalf(lista):
+    for i in range( len(lista)-1 ): print("".join( lista[:i+1] ))
+def lastHalf(lista):
+    for i in range( len(lista) ): print("".join( lista[i:] ))
+makeTriangle(4)
